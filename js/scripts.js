9904296c85e4258d976e1d69e87772c8dd0c31a7
Enock Mukhebi <mukhebienock@gmail.com>
2017-06-06 17:21:19 +0300

    change name calculator.html to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,28 @@ var add = function(number1, number2) {
 return number1 + number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-alert(add(number1, number2));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var remainder = function(number1, number2) {
+  return number1 % number2;
+};
+
+
+
+$(document).ready(function() {
+  $("form#add").submit(function() {
+   var number1 = parseInt($());
+   var number2 = parseInt(prompt($());
+   var result = 
+  });
+});
